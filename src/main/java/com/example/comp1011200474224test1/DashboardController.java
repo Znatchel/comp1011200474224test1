610f6fc3577193b1a8d9143d96d9190b01258ef4
Znatchel <Mitchellryan.work@gmail.com>
2022-10-19 10:56:25 -0400

    Question 2b complete

--- src/main/java/com/example/comp1011200474224test1/DashboardController.java
+++ src/main/java/com/example/comp1011200474224test1/DashboardController.java
@@ -1,14 +1,66 @@
 package com.example.comp1011200474224test1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class DashboardController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label Label1;
+
+    @FXML
+    private Label Label2;
+
+    @FXML
+    private TableColumn<Netflix, String> cast;
+
+    @FXML
+    private ComboBox<?> comboBox;
+
+    @FXML
+    private TableColumn<Netflix, String> director;
+
+    @FXML
+    private CheckBox moviesBox;
+
+    @FXML
+    private TableColumn<Netflix, String> rating;
+
+    @FXML
+    private TableColumn<Netflix, Integer> showId;
+
+    @FXML
+    private TableView<Netflix> tableView;
+
+    @FXML
+    private TableColumn<Netflix, String> title;
+
+    @FXML
+    private CheckBox tvShowsBox;
+
+    @FXML
+    private TableColumn<Netflix, String> type;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Here");
+        showId.setCellValueFactory(new PropertyValueFactory<>("showId"));
+        type.setCellValueFactory(new PropertyValueFactory<>("type"));
+        title.setCellValueFactory(new PropertyValueFactory<>("title"));
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        director.setCellValueFactory(new PropertyValueFactory<>("director"));
+
+        System.out.println(DBUtility.getFlixFromDB().size());
+        tableView.getItems().addAll(DBUtility.getFlixFromDB());
+
     }
 }
